2f578679c021fbecd305617f6c76368149dd63cc
hoon3284 <hoon3284@icloud.com>
2021-08-18 19:06:25 +0900

    add SystemViewControllers

--- Unit 1 - Tables And Persistence/PersistencePractice.playground/Contents.swift
+++ Unit 1 - Tables And Persistence/PersistencePractice.playground/Contents.swift
@@ -18,18 +18,36 @@ let newNote = Note(title: "Grocery run", text: "Pick up mayonnaise, mustard, let
 //    }
 //}
 
+//let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+//let archiveURL = documentsDirectory.appendingPathComponent("notes_test").appendingPathExtension("plist")
+//
+//
+//let propertyListEncoder = PropertyListEncoder()
+//let encodedNote = try? propertyListEncoder.encode(newNote)
+//
+//try? encodedNote?.write(to: archiveURL, options: .noFileProtection)
+//
+//let propertyListDecoder = PropertyListDecoder()
+//if let retrievedNoteData = try? Data(contentsOf: archiveURL), let decodedNote = try? propertyListDecoder.decode(Note.self, from: retrievedNoteData) {
+//    print(retrievedNoteData)
+//    print(decodedNote)
+//}
+
+let note1 = Note(title: "Note One", text: "This is a sample note.", timestamp: Date())
+let note2 = Note(title: "Note Two", text: "This is anohter sample note.", timestamp: Date())
+let note3 = Note(title: "Note Three", text: "This is yet another sample note.", timestamp: Date())
+let notes = [note1, note2, note3]
+
 let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
 let archiveURL = documentsDirectory.appendingPathComponent("notes_test").appendingPathExtension("plist")
 
-
 let propertyListEncoder = PropertyListEncoder()
-let encodedNote = try? propertyListEncoder.encode(newNote)
+let encodedNotes = try? propertyListEncoder.encode(notes)
 
-try? encodedNote?.write(to: archiveURL, options: .noFileProtection)
+try? encodedNotes?.write(to: archiveURL, options: .noFileProtection)
 
 let propertyListDecoder = PropertyListDecoder()
-if let retrievedNoteData = try? Data(contentsOf: archiveURL), let decodedNote = try? propertyListDecoder.decode(Note.self, from: retrievedNoteData) {
-    print(retrievedNoteData)
-    print(decodedNote)
+if let retrivedNotesData = try? Data(contentsOf: archiveURL), let decodedNotes = try? propertyListDecoder.decode(Array<Note>.self, from: retrivedNotesData) {
+    print(retrivedNotesData)
+    print(decodedNotes)
 }
-
---
+++ Unit 1 - Tables And Persistence/SystemViewControllers/SystemViewControllers/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by wickedRun on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func CameraButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+        
+    }
+}
+
