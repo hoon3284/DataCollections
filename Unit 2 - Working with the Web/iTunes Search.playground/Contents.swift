68e7326a835c77e78d9f9f854b46c52f0cfe971c
hoon3284 <hoon3284@icloud.com>
2021-08-30 16:39:26 +0900

    complete iTunes Search playground

--- Unit 2 - Working with the Web/iTunes Search.playground/Contents.swift
+++ Unit 2 - Working with the Web/iTunes Search.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+urlComponents.queryItems = [
+    "term": "IU",
+    "media": "music",
+].map { URLQueryItem(name: $0.key, value: $0.value) }
+
+let task = URLSession.shared.dataTask(with: urlComponents.url!) { (data, response, error) in
+    if let data = data, let string = String(data: data, encoding: .utf8) {
+        print(string)
+    }
+    PlaygroundPage.current.finishExecution()
+}
+
+task.resume()
