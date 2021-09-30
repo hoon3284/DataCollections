59c9dd1c59c96ad8bf0a3329d30fa966bd4e1321
wickedRun <hoon3284@icloud.com>
2021-10-01 02:18:25 +0900

    add and complete iTunesSearch project at Unit 3

---
+++ Unit 3 - Advanced Data Display/iTunesSearch/iTunesSearch/ItemCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCollectionViewCell.swift
+//  iTunesSearch
+//
+//  Created by wickedRun on 2021/10/01.
+//
+
+import UIKit
+
+class ItemCollectionViewCell: UICollectionViewCell, ItemDisplaying {
+    
+    @IBOutlet var itemImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var detailLabel: UILabel!
+    
+}
